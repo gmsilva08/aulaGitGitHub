e6f24a2f0447d7267608f950aa840e63e915bc8b
Gabriel Marcelino Silva <gmsilva08@gmail.com>
2018-09-26 14:58:11 -0300

    Primeiro commit do projeto: ler dois inteiros informados pelo usuário.

--- arq.js
+++ arq.js
@@ -1,13 +1,9 @@
 function soma() {
-	var n1, n2, soma;
+	var n1, n2;
 
 	n1 = document.getElementById("primeiroNumero").value;
 	n2 = document.getElementById("segundoNumero").value;
 
 	n1 = parseInt (n1);
 	n2 = parseInt (n2);
-
-	soma = n1 + n2;
-
-	document.getElementById("resposta").innerHTML = soma;
 }
